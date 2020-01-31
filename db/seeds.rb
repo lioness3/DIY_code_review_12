728d2c34d615c7bdea0206de4f34b309526d11ca
lioness3 <jcnailtech3@yahoo.com>
2020-01-31 17:41:55 -0500

    create fake data in seed file using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@ class Seed
 
   def generate_residents
     20.times do |i|
-      resident = Quote.create!(
-        author: Faker::Book.author,
-        content: Faker::Movie.quote
+      resident = Resident.create!(
+        name: Faker::Name.unique.name ,
+        room_number: Faker::Number.unique.within(range: 10..50),
+        age: Faker::Number.within(range: 60..100)
+        fall_risk: Faker::Boolean.boolean
       )
-      puts "Resident #{i}: Name is #{quote.author} and quotation is '#{quote.content}'."
+      puts "Resident #{i}: #{resident.name}, age #{resident.age}, is located in room #{resident.room_number}. mobile stability = #{resident.fall_risk}."
     end
   end
 end
