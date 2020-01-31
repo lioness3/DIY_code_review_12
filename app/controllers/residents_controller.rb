472c6fb784fc5199aafc3cdddc32ae3ef688bc2a
lioness3 <jcnailtech3@yahoo.com>
2020-01-31 18:28:41 -0500

    edit residents controller  to include update and destroy

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,13 +1,44 @@
 class ResidentsController < ApplicationController
 
   def index
-    @residents = {"name": "Joe Shmo", "age": 44, "room
-      -number": 233, "fall_risk": true}
+    @residents = Resident.all
     json_response(@residents)
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+end
+
+
+
+  def show
+    @resident = Resident.find(params[:id])
+    json_response(@resident)
+  end
+
+  def create
+    @resident = Resident.create(resident_params)
+    json_response(@resident)
+  end
+
+  def update
+    @resident = Resident.find(params[:id])
+    @resident.update(resident_params)
+  end
+
+  def destroy
+    @resident = Resident.find(params[:id])
+    @resident.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def resident_params
+    params.permit(:name, :room_number, :age, :fall_risk )
   end
 end
