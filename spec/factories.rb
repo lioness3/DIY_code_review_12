abdff1a4d9d1fe5472c0609f78ebef43452af247
lioness3 <jcnailtech3@yahoo.com>
2020-02-02 12:04:31 -0800

    trroubleshoot uninitilized constant error in facotry bot

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,17 @@
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
+require 'factory_bot_rails'
+
+# RSpec.configure do |config|
+#   config.include FactoryBot::Syntax::Methods
+# end
+FactoryBot.define do
+  20.times do
+  factory(:resident) do
+      resident = Resident.create!(
+        name: Faker::Name.unique.name ,
+        room_number: Faker::Number.unique.within(range: 10..50),
+        age: Faker::Number.within(range: 60..100),
+        fall_risk: Faker::Boolean.boolean
+      )
+  end
+end
 end
