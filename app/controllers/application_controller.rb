22ca70928a2346b754a8e27c7718d8f58fd57c6e
lioness3 <jcnailtech3@yahoo.com>
2020-02-02 11:32:35 -0800

    change routes.rb to residents instead of patiences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  inclide Response
+  include Response
   rescue_from ActiveRecord::RecordNotFound do |exception|
   json_response({ message: exception.message }, :not_found)
 end
