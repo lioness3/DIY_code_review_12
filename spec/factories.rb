29fe308d4e84db047440ae3103abff47997531f5
lioness3 <jcnailtech3@yahoo.com>
2020-02-02 12:12:15 -0800

    remove constant Resident in factories.rb and use faker instead

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,14 +4,11 @@ require 'factory_bot_rails'
 #   config.include FactoryBot::Syntax::Methods
 # end
 FactoryBot.define do
-  20.times do
   factory(:resident) do
-      resident = Resident.create!(
-        name: Faker::Name.unique.name ,
-        room_number: Faker::Number.unique.within(range: 10..50),
-        age: Faker::Number.within(range: 60..100),
-        fall_risk: Faker::Boolean.boolean
-      )
+        name { Faker::Name.unique.name }
+        room_number  { Faker::Number.unique.within(range: 10..50) }
+        age { Faker::Number.within(range: 60..100) }
+        fall_risk { Faker::Boolean.boolean }
+
   end
-end
 end
